6fc86296457f4b6787bd17b16b70728844288350
valevalorin <leucochroic@gmail.com>
2017-10-29 15:03:40 -0500

    now works with looping and has a transition buffer, with logic for queuing and dequeuing transition based on current player state

--- app.js
+++ app.js
@@ -5,40 +5,76 @@ var util = require('util');
 var crossFadeEasing = require('eases/cubic-out');
 var Config = require("./config.js");
 
-var app = express(); 
-var player = new MPlayer();
+var app = express();
 var config = Config.load();
+var player = new MPlayer();
+var manuallyStopped = false;
+var queuedAssignment = null;
+var currentAssignment = null;
+var transitionTimeout = null;
+
+player.on('stop', function (status) {
+	if(!manuallyStopped) {
+		player.openFile(currentAssignment.trackPath);
+	}
+	manuallyStopped = false;
+});
 
 app.use(bodyParser.json());
 
 app.post('/process-changed', function (req, res) {
-	var assignment = config.assignmentsMap[req.body.processName];
-	if(assignment) {
-		console.log("I will play: " + assignment.trackPath);
-		if(!player.status.filename) {
-			player.openFile(assignment.trackPath);
-		} else {
-			var intervalCounter = 0;
-			var maxTime = config.crossFade.intervalTime * config.crossFade.intervalLoops;
-			var interval = setInterval(function () {
-				if(intervalCounter >= config.crossFade.intervalLoops) {
-					clearInterval(interval);
-					player.stop();
-					player.openFile(assignment.trackPath);
-				} else {
-					intervalCounter = intervalCounter + 1;
-					var volume = (maxTime - (intervalCounter * config.crossFade.intervalTime))/maxTime;
-					volume = crossFadeEasing(volume);
-					player.volume(volume * 100);
-				}
-			}, config.crossFade.intervalTime); 
+	if(!player.status.filename) {
+		console.log(req.body.processName);
+		if(config.assignmentsMap[req.body.processName].trackPath) {
+			player.openFile(config.assignmentsMap[req.body.processName].trackPath);
+			currentAssignment = config.assignmentsMap[req.body.processName];
 		}
 	} else {
-		if(!config.passThroughsMap[req.body.processName]) {
-			player.stop();
+		if(!queuedAssignment || req.body.processName != queuedAssignment.processName) {
+			clearTimeout(transitionTimeout);
+			transitionTimeout = null;
+		}
+		
+
+		if((!queuedAssignment || req.body.processName != queuedAssignment.processName) && req.body.processName != currentAssignment.processName ) {
+			queuedAssignment = config.assignmentsMap[req.body.processName];
+			transitionTimeout = setTimeout(function () {
+				if(queuedAssignment) {
+					var intervalCounter = 0;
+					var maxTime = config.crossFade.intervalTime * config.crossFade.intervalLoops;
+					var interval = setInterval(function () {
+						if(intervalCounter >= config.crossFade.intervalLoops) {
+							clearInterval(interval);
+							manuallyStopped = true;
+							player.stop();
+							player.openFile(queuedAssignment.trackPath);
+							currentAssignment = queuedAssignment;
+							queuedAssignment = null;
+						} else {
+							intervalCounter = intervalCounter + 1;
+							var volume = (maxTime - (intervalCounter * config.crossFade.intervalTime))/maxTime;
+							volume = crossFadeEasing(volume);
+							player.volume(volume * 100);
+						}
+					}, config.crossFade.intervalTime); 
+				} else {
+					if(!config.passThroughsMap[req.body.processName]) {
+						manuallyStopped = true;
+						player.stop();
+					}
+				}
+			}, config.transitionBuffer * 1000);
+		} else {
+			queuedAssignment = null;
 		}
 	}
 
+	res.send(JSON.stringify({success: true}));
+});
+
+app.get('/stop', function (req, res) {
+	manuallyStopped = true;
+	player.stop();
 	res.send(JSON.stringify({success: true}));
 });
 
--- config.js
+++ config.js
@@ -7,15 +7,19 @@ function load() {
 	config.crossFade.intervalLoops = (1000 * config.crossFade.seconds)/config.crossFade.intervalTime;
 
 	config.assignmentsMap = {};
-	for (var i = config.assignments.length - 1; i >= 0; i--) {
-		var assignment = config.assignments[i];
-		assignment.trackPath = assignment.package.replace("package.ostmanifest", assignment.track).replace(/\\/g, "\\\\");
-		config.assignmentsMap[assignment.processName] = assignment;
+	if(config.assignments) {
+		for (var i = config.assignments.length - 1; i >= 0; i--) {
+			var assignment = config.assignments[i];
+			assignment.trackPath = assignment.package.replace("package.ostmanifest", assignment.track).replace(/\\/g, "\\\\");
+			config.assignmentsMap[assignment.processName] = assignment;
+		}
 	}
 
 	config.passThroughsMap = {};
-	for (var i = config.passThroughs.length - 1; i >= 0; i--) {
-		config.passThroughsMap[config.passThroughs[i]] = true;
+	if(config.passThroughs) {
+		for (var i = config.passThroughs.length - 1; i >= 0; i--) {
+			config.passThroughsMap[config.passThroughs[i]] = true;
+		}
 	}
 
 	return config;
