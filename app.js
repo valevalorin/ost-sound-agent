2ff52265d14b46b913d6a461f6a125eb5292846f
valevalorin <leucochroic@gmail.com>
2017-10-29 15:09:41 -0500

    removed debug logging, fade out when transisitioning to non-assigned non-passthrough process

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var queuedAssignment = null;
 var currentAssignment = null;
 var transitionTimeout = null;
 
+function crossFade(){
+
+}
+
 player.on('stop', function (status) {
 	if(!manuallyStopped) {
 		player.openFile(currentAssignment.trackPath);
@@ -24,7 +28,6 @@ app.use(bodyParser.json());
 
 app.post('/process-changed', function (req, res) {
 	if(!player.status.filename) {
-		console.log(req.body.processName);
 		if(config.assignmentsMap[req.body.processName].trackPath) {
 			player.openFile(config.assignmentsMap[req.body.processName].trackPath);
 			currentAssignment = config.assignmentsMap[req.body.processName];
@@ -38,8 +41,9 @@ app.post('/process-changed', function (req, res) {
 
 		if((!queuedAssignment || req.body.processName != queuedAssignment.processName) && req.body.processName != currentAssignment.processName ) {
 			queuedAssignment = config.assignmentsMap[req.body.processName];
+			var isPassThrough = config.passThroughsMap[req.body.processName];
 			transitionTimeout = setTimeout(function () {
-				if(queuedAssignment) {
+				if(queuedAssignment || !isPassThrough) {
 					var intervalCounter = 0;
 					var maxTime = config.crossFade.intervalTime * config.crossFade.intervalLoops;
 					var interval = setInterval(function () {
@@ -47,9 +51,11 @@ app.post('/process-changed', function (req, res) {
 							clearInterval(interval);
 							manuallyStopped = true;
 							player.stop();
-							player.openFile(queuedAssignment.trackPath);
-							currentAssignment = queuedAssignment;
-							queuedAssignment = null;
+							if(queuedAssignment) {
+								player.openFile(queuedAssignment.trackPath);
+								currentAssignment = queuedAssignment;
+								queuedAssignment = null;
+							}
 						} else {
 							intervalCounter = intervalCounter + 1;
 							var volume = (maxTime - (intervalCounter * config.crossFade.intervalTime))/maxTime;
@@ -57,11 +63,6 @@ app.post('/process-changed', function (req, res) {
 							player.volume(volume * 100);
 						}
 					}, config.crossFade.intervalTime); 
-				} else {
-					if(!config.passThroughsMap[req.body.processName]) {
-						manuallyStopped = true;
-						player.stop();
-					}
 				}
 			}, config.transitionBuffer * 1000);
 		} else {
